f8c5a6facffee2932abfed859deceb0d129cc412
Joshua McMillan <jmcmillan.programming@gmail.com>
2020-02-02 02:04:56 -0600

    Adds player movement
    Player can move, rotate, and snap rotate. Added graphics.

---
+++ NokiaTempleCrawler/Assets/Controls.cs
@@ -0,0 +1,351 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""747a887d-2694-439a-b53f-cc62335e90ad"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""cc07a6cf-b941-4773-919e-0286f64dd08b"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Rotate"",
+                    ""type"": ""Value"",
+                    ""id"": ""34441419-a0ad-4d2b-b82e-e308cf91dd81"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""SnapLeft"",
+                    ""type"": ""Button"",
+                    ""id"": ""80b206f5-5718-4a0e-b2f4-30b8d4e4f5b6"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""SnapRight"",
+                    ""type"": ""Button"",
+                    ""id"": ""cb33013b-4616-48bf-9c45-47617a31c62a"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""NumberPad"",
+                    ""id"": ""effb1d58-7567-4461-9c5e-95966e896432"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""a042dcd8-f7a0-4b7b-86be-e8f6393e923d"",
+                    ""path"": ""<Keyboard>/numpad5"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""dfa0ca87-9c61-4fc8-9f39-34905937e1d3"",
+                    ""path"": ""<Keyboard>/numpad8"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""97214f68-efe1-4ea6-abb6-6c43c6aa20b7"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""6c3128fb-1069-4e71-a26c-9af65e85b3a9"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""b006da90-9f86-469c-bb13-f51018b50639"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Numberpad"",
+                    ""id"": ""0a9ec52a-12b3-4abc-a9c9-318ad59aeb99"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""f3a609fc-4fbe-4c1c-8a10-93976570aaf9"",
+                    ""path"": ""<Keyboard>/numpad4"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""13936d22-0f09-43db-be05-6db18803f8c8"",
+                    ""path"": ""<Keyboard>/numpad6"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Keyboard"",
+                    ""id"": ""7e7444b5-f238-4e21-8529-8fddf87963a1"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""b72b68e8-4e11-45e4-96da-8bdea4e52532"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""adc3fb18-d60a-432b-8eed-cdac62761544"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3acd713a-f138-4349-87bd-639fadfcd0c8"",
+                    ""path"": ""<Keyboard>/numpad7"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SnapLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9900f93d-3f85-442a-9747-5ff54a19ceb0"",
+                    ""path"": ""<Keyboard>/q"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SnapLeft"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""169c283c-b504-4aa6-a2ac-c03c87f22646"",
+                    ""path"": ""<Keyboard>/numpad9"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SnapRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5811d1d4-2a88-4054-b934-8c018e2faf98"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SnapRight"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+        m_Player_Rotate = m_Player.FindAction("Rotate", throwIfNotFound: true);
+        m_Player_SnapLeft = m_Player.FindAction("SnapLeft", throwIfNotFound: true);
+        m_Player_SnapRight = m_Player.FindAction("SnapRight", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    private readonly InputAction m_Player_Rotate;
+    private readonly InputAction m_Player_SnapLeft;
+    private readonly InputAction m_Player_SnapRight;
+    public struct PlayerActions
+    {
+        private @Controls m_Wrapper;
+        public PlayerActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputAction @Rotate => m_Wrapper.m_Player_Rotate;
+        public InputAction @SnapLeft => m_Wrapper.m_Player_SnapLeft;
+        public InputAction @SnapRight => m_Wrapper.m_Player_SnapRight;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Rotate.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotate;
+                @Rotate.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotate;
+                @Rotate.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnRotate;
+                @SnapLeft.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSnapLeft;
+                @SnapLeft.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSnapLeft;
+                @SnapLeft.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSnapLeft;
+                @SnapRight.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSnapRight;
+                @SnapRight.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSnapRight;
+                @SnapRight.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSnapRight;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Rotate.started += instance.OnRotate;
+                @Rotate.performed += instance.OnRotate;
+                @Rotate.canceled += instance.OnRotate;
+                @SnapLeft.started += instance.OnSnapLeft;
+                @SnapLeft.performed += instance.OnSnapLeft;
+                @SnapLeft.canceled += instance.OnSnapLeft;
+                @SnapRight.started += instance.OnSnapRight;
+                @SnapRight.performed += instance.OnSnapRight;
+                @SnapRight.canceled += instance.OnSnapRight;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnRotate(InputAction.CallbackContext context);
+        void OnSnapLeft(InputAction.CallbackContext context);
+        void OnSnapRight(InputAction.CallbackContext context);
+    }
+}
---
+++ NokiaTempleCrawler/Assets/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private float rotateSpeed = 3.0f;
+
+    private CharacterController controller;
+
+    private Controls controls = null;
+
+    private void Awake()
+    {
+        controls = new Controls();
+    }
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable() => controls.Player.Enable();
+
+    private void OnDisable() => controls.Player.Disable();
+
+    private void Update()
+    {
+        Rotate();
+        Move();
+    }
+
+    private void Move()
+    {
+        float movement_in = controls.Player.Move.ReadValue<float>();
+
+        Vector3 movement = new Vector3(0.0f, 0.0f, movement_in);
+
+        movement = transform.TransformDirection(movement);
+
+        movement.Normalize();
+
+        controller.SimpleMove(movement * moveSpeed);
+    }
+
+    private void Rotate()
+    {
+        float rotation_in = controls.Player.Rotate.ReadValue<float>();
+
+        Vector3 rotation = new Vector3(0.0f, rotation_in, 0.0f);
+
+        transform.Rotate(rotation * rotateSpeed);
+    }
+
+    public void SnapLeft()
+    {
+        transform.Rotate(0.0f, -45.0f, 0.0f);
+    }
+
+    public void SnapRight()
+    {
+        transform.Rotate(0.0f, 45.0f, 0.0f);
+    }
+}
