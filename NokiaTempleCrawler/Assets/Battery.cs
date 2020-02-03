2ec4630fc8641ef5e8bcb1e5b9abc4b195a9dc4d
Joshua McMillan <jmcmillan.programming@gmail.com>
2020-02-03 14:22:57 -0600

    Adds battery mechanic
    Battery slowly drains and updates UI

---
+++ NokiaTempleCrawler/Assets/Battery.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BatteryState
+{
+    FULL,
+    GOOD,
+    OKAY,
+    LOW,
+    DEAD
+}
+
+public class Battery : MonoBehaviour
+{
+    public static event Action<BatteryState> OnBatteryUpdated;
+
+    [SerializeField] private float batteryTime = 300.0f;
+
+    private WaitForSecondsRealtime waitTime;
+
+    private BatteryState currentState;
+
+    private void Start()
+    {
+        waitTime = new WaitForSecondsRealtime(batteryTime);
+
+        StartCoroutine(BatteryTimer());
+    }
+
+    IEnumerator BatteryTimer()
+    {
+        yield return waitTime;
+
+        switch (currentState)
+        {
+            case BatteryState.FULL:
+                currentState = BatteryState.GOOD;
+                break;
+
+            case BatteryState.GOOD:
+                currentState = BatteryState.OKAY;
+                break;
+
+            case BatteryState.OKAY:
+                currentState = BatteryState.LOW;
+                break;
+
+            case BatteryState.LOW:
+                currentState = BatteryState.DEAD;
+                break;
+
+            case BatteryState.DEAD:
+                Debug.Log("GAME OVER!");
+                break;
+        }
+
+        OnBatteryUpdated(currentState);
+
+        StartCoroutine(BatteryTimer());
+    }
+}
---
+++ NokiaTempleCrawler/Assets/BatteryUI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryUI : MonoBehaviour
+{
+    [Header("Signal Sprites")]
+    [SerializeField] private Sprite fullBattery;
+    [SerializeField] private Sprite goodBattery;
+    [SerializeField] private Sprite okayBattery;
+    [SerializeField] private Sprite lowBattery;
+    [SerializeField] private Sprite deadBattery;
+
+    private Image batteryIndicator;
+
+    private void Start()
+    {
+        batteryIndicator = GetComponent<Image>();
+
+        batteryIndicator.sprite = fullBattery;
+    }
+
+    private void OnEnable()
+    {
+        Battery.OnBatteryUpdated += UpdateUI;
+    }
+
+    private void OnDisable()
+    {
+        Battery.OnBatteryUpdated -= UpdateUI;
+    }
+
+    private void UpdateUI(BatteryState strength)
+    {
+        switch (strength)
+        {
+            case BatteryState.FULL:
+                batteryIndicator.sprite = fullBattery;
+                break;
+
+            case BatteryState.GOOD:
+                batteryIndicator.sprite = goodBattery;
+                break;
+
+            case BatteryState.OKAY:
+                batteryIndicator.sprite = okayBattery;
+                break;
+
+            case BatteryState.LOW:
+                batteryIndicator.sprite = lowBattery;
+                break;
+
+            case BatteryState.DEAD:
+                batteryIndicator.sprite = deadBattery;
+                break;
+
+            default:
+                batteryIndicator.sprite = fullBattery;
+                break;
+        }
+    }
+}
